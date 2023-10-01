d6c51955f24a14de11d39af872951483b1d6ca52
artiom.oriol <orlis4e@gmail.com>
2023-10-01 16:04:45 +0300

    added the constants...

---
+++ src/test/java/Constants/TextValues.java
@@ -0,0 +1,5 @@
+package Constants;
+
+public class TextValues {
+    public static final String SOME_STRING_VALUE = "";
+}
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,7 +1,10 @@
+import Constants.TextValues;
 import Core.BaseSeleniumTest;
 import org.junit.Test;
 
 public class MainTest extends BaseSeleniumTest {
+
+    String someStringValue = TextValues.SOME_STRING_VALUE;
 
     @Test
     public void mainTest(){
